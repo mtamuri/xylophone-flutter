46af4cbe5f3e813f8f25de1885a43cd8d5e0f3ca
Ammar <101336592+mtamuri@users.noreply.github.com>
2024-09-11 18:44:10 +0300

    changes before i ruin android files

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:audioplayers/audioplayers.dart';
 
 void main() => runApp(XylophoneApp());
 
 class XylophoneApp extends StatelessWidget {
+  final player = AudioPlayer();
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
         body: SafeArea(
-          child: Container(),
+          child: Center(
+            child: TextButton(
+              onPressed: () {
+                player.play(AssetSource('assets/note1.wav'));
+              },
+              child: Text('Play'),
+            ),
+          ),
         ),
       ),
     );
